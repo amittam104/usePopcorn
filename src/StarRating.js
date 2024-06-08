fa706d5112d526f9786b6eeb572e12037095adea
Amit Tambulkar <amittambulkar104@gmail.com>
2024-06-08 20:22:08 +0530

    chore: update rating component

--- src/StarRating.js
+++ src/StarRating.js
@@ -12,55 +12,70 @@ const ratingStartContainer = {
   gap: "4px",
 };
 
-const TextStyle = {
-  lineHeight: "1",
-  margin: "0",
-};
-
-const startStyle = {
-  width: "24px",
-  height: "24px",
-};
-
-export default function StarRating({ maxRating = 5 }) {
-  const [rating, setRating] = useState(0);
+export default function StarRating({
+  maxRating = 5,
+  size = "24",
+  color = "#fcc419",
+  messages = [],
+  className = "",
+  defaultRating = 0,
+  onRating,
+}) {
+  const [rating, setRating] = useState(defaultRating);
   const [tempRating, SetTempRating] = useState(0);
 
-  // function handleRating() {
-  //   setRating();
-  // }
+  const TextStyle = {
+    lineHeight: "1",
+    margin: "0",
+    color,
+    fontSize: `${size / 1.2}px`,
+    fontFamily: "sans-serif",
+  };
+
+  function handleSetRating(rating) {
+    setRating(rating);
+    onRating(rating);
+  }
 
   return (
-    <div style={ratingContainerStyle}>
+    <div style={ratingContainerStyle} className={className}>
       <div style={ratingStartContainer}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
-            onRate={() => setRating(i + 1)}
+            onRate={() => handleSetRating(i + 1)}
             isRated={tempRating ? tempRating >= i + 1 : rating >= i + 1}
             onMouseIn={() => SetTempRating(i + 1)}
             onMouseOut={() => SetTempRating(0)}
+            color={color}
+            size={size}
           />
         ))}
       </div>
-      <p style={TextStyle}>{tempRating || rating || ""}</p>
+      <p style={TextStyle}>
+        {messages.length === maxRating
+          ? messages[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ""}
+      </p>
     </div>
   );
 }
 
-function Star({ onRate, isRated, onMouseIn, onMouseOut }) {
+function Star({ onRate, isRated, onMouseIn, onMouseOut, color, size }) {
+  const startStyle = {
+    width: `${size}px`,
+    height: `${size}px`,
+    color,
+  };
+
   return (
-    <div
-      style={startStyle}
-      onClick={onRate}
-      onMouseEnter={onMouseIn}
-      onMouseLeave={onMouseOut}
-    >
+    <div onClick={onRate} onMouseEnter={onMouseIn} onMouseLeave={onMouseOut}>
       {isRated ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill="#000"
-          stroke="#000"
+          fill={color}
+          stroke={color}
+          style={startStyle}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
@@ -69,7 +84,8 @@ function Star({ onRate, isRated, onMouseIn, onMouseOut }) {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="#000"
+          stroke={color}
+          style={startStyle}
         >
           <path
             strokeLinecap="round"
--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating maxRating={10} onRating={setMovieRating} />
+      <p>The movie rating is {movieRating} star</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating maxRating={10} />
+    <StarRating
+      maxRating={5}
+      size="32"
+      color="blue"
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      className="test"
+      defaultRating={3}
+    />
+    <Test />
   </React.StrictMode>
 );
