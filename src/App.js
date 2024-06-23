5018ccff1d1c151f9f2a7ce117c4fcbe9ef16e99
Amit Tambulkar <amittambulkar104@gmail.com>
2024-06-23 07:47:31 +0530

    chore: add wathed list to local storage and fetch at initial render

--- src/App.js
+++ src/App.js
@@ -9,7 +9,11 @@ const average = (arr) =>
 export default function App() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
+  // const [watched, setWatched] = useState([]);
+  const [watched, setWatched] = useState(function () {
+    const getStorageData = localStorage.getItem("watched");
+    return JSON.parse(getStorageData);
+  });
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
@@ -29,6 +33,13 @@ export default function App() {
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
+
+  useEffect(
+    function () {
+      localStorage.setItem("watched", JSON.stringify(watched));
+    },
+    [watched]
+  );
 
   useEffect(
     function () {
